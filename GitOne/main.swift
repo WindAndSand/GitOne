266f3c994eac2d98e3ee20c3386191aab07bb4c0
WindAndSand <2711722020@qq.com>
2017-08-15 00:36:10 +0800

    创建 main.m 文件

---
+++ GitOne/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitOne
+//
+//  Created by HuangGang on 2017/8/15.
+//  Copyright © 2017年 HuangGang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
